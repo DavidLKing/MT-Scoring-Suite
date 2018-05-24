cac7473f52f43583897b1bfbf692f8e4d54291e7
David L. King <david.king@lengoo.de>
2018-05-24 15:35:35 +0200

    finalizing

--- README.md
+++ README.md
@@ -9,4 +9,9 @@ Set `config.yml` to have the location of your METEOR installation.
 
 Get scores with:
 
-`./score.py -c config.yml -r gold.txt -p pred.txt -o test.txt`
+`./score.py -c config.yml -s src.txt -r gold.txt -p pred.txt -o test.tsv`
+
+Be sure to configure config.yml to reflect the ABSOLUTE paths of where you 
+downloaded [METEOR](http://www.cs.cmu.edu/~alavie/METEOR/) and 
+[TER](http://www.cs.umd.edu/~snover/tercom/). Also be sure to set the language
+for METEOR.
--- score.py
+++ score.py
@@ -14,6 +14,7 @@ import subprocess
 parser = argparse.ArgumentParser()
 requiredNamed = parser.add_argument_group('required named arguments')
 requiredNamed.add_argument('-c', '--config', help='YaML config file', required=True)
+requiredNamed.add_argument('-s', '--src', help='Source file', required=True)
 requiredNamed.add_argument('-r', '--ref', help='Reference file', required=True)
 requiredNamed.add_argument('-p', '--pred', help='Prediction file', required=True)
 requiredNamed.add_argument('-o', '--output', help='Output file', required=True)
@@ -29,6 +30,8 @@ class Score:
         I'm not sure if we should use arguments or a configuration file for this
         """
         self.args = args
+        # Gross one-liner. I'm so sorry!
+        self.ter2txt = lambda x: x.stdout.decode().split('\n')[4].replace("Total TER: ", "").split(' ')[0]
 
 
     def load_file(self, _file):
@@ -37,6 +40,25 @@ class Score:
         If these files get too long, this may get RAM intensive
         """
         return open(_file, 'r').readlines()
+
+    def prepTER(self, predfile, reffile):
+        preds = open(predfile, 'r').readlines()
+        refs = open(reffile, 'r').readlines()
+        predTER = predfile + "TER"
+        refTER = reffile + "TER"
+        self.ter_write(predTER, preds)
+        self.ter_write(refTER, refs)
+
+    def ter_write(self, _file, lines):
+        i = 0
+        with open(_file, 'w') as f:
+            for l in lines:
+                l = l.strip()
+                l += ' ('
+                l += str(i)
+                l += ')\n'
+                i += 1
+            f.write(l)
 
     def bleu_weights(self, maxn):
         weights = []
@@ -53,7 +75,41 @@ class Score:
         assert(len(scores) == len(weights))
         return scores
 
-    def main(self, reffile, predfile, maxn):
+    def lineTER(self, pred, ref):
+        p = open('temp.pred', 'w')
+        r = open('temp.ref', 'w')
+        predLine = pred.strip() + ' ' + "(1)"
+        refLine = ref.strip() + ' ' + "(1)"
+        p.write(predLine)
+        r.write(refLine)
+        p.close()
+        r.close()
+        tercmd = [
+            'java',
+            '-jar',
+            config['TER'],
+            '-h',
+            "temp.pred",
+            '-r',
+            "temp.ref"
+        ]
+        ter_out = subprocess.run(tercmd, stdout=subprocess.PIPE)
+        return self.ter2txt(ter_out)
+
+    def get_met_sent(self, meteor):
+        meteor = meteor.stdout.decode().split("\n")
+        # List comprehension isn't working
+        # test = [x.startswith("Segment") for x in meteor]
+        scores = []
+        for met in meteor:
+            if met.startswith("Segment"):
+                met = met.split("\t")[1]
+                scores.append(met)
+        return scores
+
+    def main(self, srcfile, reffile, predfile, maxn, out):
+        self.prepTER(predfile, reffile)
+        srcs = s.load_file(srcfile)
         refs = s.load_file(reffile)
         preds = s.load_file(predfile)
         assert(len(refs) == len(preds)), "Reference and prediciton files must be of the same length"
@@ -69,22 +125,69 @@ class Score:
             reffile,
             predfile,
             "-l",
-            "en"
+            config['METLANG']
         ]
-        print("Starting meteor")
+        print("Starting METEOR")
         meteor = subprocess.run(cmd, stdout=subprocess.PIPE)
+        met_sent = self.get_met_sent(meteor)
+        print("Starting TER")
+        tercmd = [
+            'java',
+            '-jar',
+            config['TER'],
+            '-h',
+            predfile + 'TER',
+            '-r',
+            reffile + 'TER'
+        ]
+        ter_corp = subprocess.run(tercmd, stdout=subprocess.PIPE)
+        ter_corp = self.ter2txt(ter_corp)
         sent_scores = []
+        ter_scores = []
+        print("Getting individual TER and BLEU scores")
         for ref, pred in zip(refs, preds):
             scores = self.get_bleus(ref, pred, weight_tuples)
             sent_scores.append(scores)
-        pdb.set_trace()
-
+            ter_out = self.lineTER(pred, ref)
+            ter_scores.append(ter_out)
+        # Sanity check:
+        assert(len(ter_scores) == len(sent_scores))
+        assert(len(met_sent) == len(sent_scores))
+        header = [
+            "Source",
+            "Human",
+            "Machine",
+            "TER",
+            "METEOR",
+            "BLEU 1"
+            "BLEU 2"
+            "BLEU 3"
+            "BLEU 4"
+        ]
+        outfile = open(out, 'w')
+        outfile.write('\t'.join(header) + '\n')
+        for src, ref, pred, ter, met, bleu in zip(srcs, refs, preds, ter_scores, met_sent, sent_scores):
+            outline = [src, ref, pred, ter, met]
+            # pdb.set_trace()
+            outline += bleu
+            # pdb.set_trace()
+            outline = [str(x) for x in outline]
+            # pdb.set_trace()
+            outline = [x.strip() for x in outline]
+            # pdb.set_trace()
+            outline = '\t'.join(outline)
+            # pdb.set_trace()
+            outline += '\n'
+            # pdb.set_trace()
+            outfile.write(outline)
 
 
 
 if __name__ == '__main__':
     s = Score()
+    src = s.args.src
     ref = s.args.ref
     pred = s.args.pred
-    s.main(ref, pred, config['BLEU n-gram'])
+    out = s.args.output
+    s.main(src, ref, pred, config['BLEU n-gram'], out)
 
